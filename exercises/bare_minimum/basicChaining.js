954a0a9d34e7b9b7c1a5f2e796caa4a3c5fba634
Chris Hesterman <chrishesterman14@gmail.com>
2020-04-16 14:24:13 -0700

    "Complete Bare Minimum Requirements"

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -8,16 +8,32 @@
  * HINT: We exported some similar promise-returning functions in previous exercises
  */
 
+// 1. promise constructor pluck first line (already async)
+// 2. promisification getGitHubProfile
+
 var fs = require('fs');
 var Promise = require('bluebird');
+var promisification = require('./promisification.js');
+var promiseConstructor = require('./promiseConstructor.js');
 
-
-
-var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+var fetchProfileAndWriteToFile = function (readFilePath, writeFilePath) {
+  return promiseConstructor
+    .pluckFirstLineFromFileAsync(readFilePath)
+    .then((username) => {
+      return promisification.getGitHubProfileAsync(username);
+    })
+    .then((profile) => {
+      return fs.writeFileSync(writeFilePath, JSON.stringify(profile), (err) => {
+        if (err) {
+          throw err;
+        } else {
+          console.log('Success!');
+        }
+      });
+    });
 };
 
 // Export these functions so we can test them
 module.exports = {
-  fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
+  fetchProfileAndWriteToFile: fetchProfileAndWriteToFile,
 };
