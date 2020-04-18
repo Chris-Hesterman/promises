e1defe6ff44f981cf24d749d431d64cf12102a3a
Chris Hesterman <chrishesterman14@gmail.com>
2020-04-18 11:09:03 -0700

    Complete advanced/collections.js
    Made use of promises.

--- exercises/advanced/collections.js
+++ exercises/advanced/collections.js
@@ -7,13 +7,37 @@
  *      - i.e. the second line in the new file should be the first line of `filePaths[1]`
  *    4. Writes the new file to the file located at `writePath`
  */
+var firstLine = require('../bare_minimum/promiseConstructor');
+var fs = require('fs');
+var Promise = require('bluebird');
+var appendFileP = Promise.promisify(require('fs').appendFile);
 
-
-var combineFirstLineOfManyFiles = function(filePaths, writePath) {
- // TODO
+var combineFirstLineOfManyFiles = function (filePaths, writePath) {
+  return new Promise((resolve, reject) => {
+    var pluckedFiles = filePaths.map((filePath) => {
+      return firstLine.pluckFirstLineFromFileAsync(filePath);
+    });
+    Promise.all(pluckedFiles)
+      .then((linesArray) => {
+        console.log(linesArray);
+        return linesArray.forEach((line, index, array) => {
+          if (index < array.length - 1) {
+            appendFileP(writePath, line + '\n');
+          } else {
+            appendFileP(writePath, line);
+          }
+        });
+      })
+      .then(() => {
+        resolve(writePath);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 };
 
 // Export these functions so we can unit test them
 module.exports = {
-  combineFirstLineOfManyFiles: combineFirstLineOfManyFiles
+  combineFirstLineOfManyFiles: combineFirstLineOfManyFiles,
 };
